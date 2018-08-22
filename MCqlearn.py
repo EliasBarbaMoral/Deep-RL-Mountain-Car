8f2e96d2e92c4096448c3c3b544d932f4baed819
Elias Barba Moral <elias.barbamoral@p3-group.com>
2018-08-22 10:01:08 +0200

    tuning of the memory

--- MCqlearn.py
+++ MCqlearn.py
@@ -1,6 +1,5 @@
 import json
 import random
-import itertools
 import numpy as np
 from keras.models import Sequential
 from keras.layers.core import Dense
@@ -9,34 +8,34 @@ from keras.optimizers import sgd
 import gym 
 
 class ExperienceReplay(object):
-    def __init__(self, max_memory=100, discount=.9):
+    def __init__(self, max_memory=100, max_good_memory = 100, discount=.9):
         self.max_memory = max_memory
+        self.max_good_memory = max_good_memory
         self.memory_good = list()
         self.memory_rand = list()
         self.memory = list
         self.discount = discount
 
-    def remember(self, states, game_over):
+    def remember(self, states, game_over,percentage):
         # memory[i] = [[state_t, action_t, reward_t, state_t+1], game_over?]
         self.memory_rand.append([states, game_over])
         if len(self.memory_rand) > self.max_memory:
             del self.memory_rand[0]
             
         if game_over:
-            q_to_take = np.random.randint(0,self.max_memory)
-            self.memory_good += self.memory_rand[q_to_take:]
+            self.memory_good += self.memory_rand[-self.max_good_memory:]
             
             if len(self.memory_good) > self.max_memory:
                 self.memory_good = random.sample(self.memory_good,self.max_memory)
-                
-            print("Taken Qs:")
-            print(q_to_take)
-                
-        if len(self.memory_good)==0:
+                                
+        if len(self.memory_good) == 0:
             self.memory_good = self.memory_rand
+        
+        if len(self.memory_good) == self.max_memory:
+             self.memory_rand = self.memory_good
             
-        sample_good = random.choices(self.memory_good, k = int(self.max_memory/2))
-        sample_rand = random.choices(self.memory_rand, k = int(self.max_memory/2))
+        sample_good = random.choices(self.memory_good, k = int(self.max_memory*percentage))
+        sample_rand = random.choices(self.memory_rand, k = int(self.max_memory*(1-percentage)))
         
         self.memory = sample_good+sample_rand
 
@@ -69,7 +68,7 @@ if __name__ == "__main__":
     epsilon_abs = 9
     #epsilon = .1
     num_actions = 3  # [move_left, stay, move_right]
-    epoch = 10
+    epoch = 30
     max_memory = 500
     hidden_size = 100
     batch_size = 50
@@ -99,7 +98,8 @@ if __name__ == "__main__":
     # Train
     win_cnt = 0
     for e in range(epoch):
-        epsilon = max(.1,epsilon_abs-e/(2*epoch))  # exploration
+        percentage = 0.5 + e/(2*epoch)
+        epsilon = max(.1,epsilon_abs-e/(2*epsilon_abs+1))  # exploration
         loss = 0.
         done = False
         # get initial input
@@ -133,7 +133,7 @@ if __name__ == "__main__":
 
             input_t = input_t.reshape((1,-1))
             # store experience
-            exp_replay.remember([input_tm1, action, reward, input_t], done)
+            exp_replay.remember([input_tm1, action, reward, input_t], done, percentage)
 
             # adapt model
             inputs, targets = exp_replay.get_batch(model, batch_size=batch_size)
